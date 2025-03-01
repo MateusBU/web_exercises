8171c0b681b7ff19c2fdf3bf4a58fa43a8cc9a74
Mateus Urtassum <mateusurtassum@gmail.com>
2025-03-01 15:50:39 -0300

    add nodemon (automatically restarts Node.js applications on file changes.)

--- html/exercises/form/server.js
+++ html/exercises/form/server.js
@@ -9,7 +9,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 //and send Congrats
 app.post('/users', (req, resp, next) =>{
     console.log(req.body);
-    resp.send('<h1>Congrats</h1>');
+    resp.send('<h1>Congrats. Included user</h1>');
+});
+
+app.post('/users/:id', (req, resp, next) =>{
+    console.log(req.params.id);
+    console.log(req.body);
+    resp.send(`<h1>Congrats. Altered user ${req.body.nameUser}</h1>`);
 });
 
 app.listen(3003)
